a7611b68d7e3dd02b7e82d5a76c84d5a4712eed1
pankajdev1221 <pankaj.dev1221@gmail.com>
2022-06-26 14:33:56 +0530

    Initail Components of routers

--- src/MovieInformation/MovieInformation.jsx
+++ src/MovieInformation/MovieInformation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieInformation = () => {
   return (
     <div>
-        
+        MovieInformation
     </div>
   )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { CssBaseline } from '@mui/material'
 import { Route , Routes } from 'react-router-dom'
+import { Actors , Movies , MovieInformation , Navbar , Profile } from './indexShared'
 const App = () => {
   return (
     <div>
         <CssBaseline />
-
+        <Navbar />
         <main>
             <Routes>
-                <Route path="/" element={ <h1>Home</h1>} />                   
-                <Route path="/movies" element={ <h1>Movies</h1>} />
-                <Route path="/movie/:id" element={ <h1>Movie Infromation</h1>} />
-                <Route path="/actors/:id" element={ <h1>Actorrs</h1>} />
-                <Route path="/profile/:id" element={ <h1>Profile</h1>} />
+                {/* <Route path="/" element={ <h1>Home</h1>} />                    */}
+                <Route path="/movies" element={ <Movies />} />
+                <Route path="/movie/:id" element={ <MovieInformation />} />
+                <Route path="/actors/:id" element={<Actors />} />
+                <Route path="/profile/:id" element={ <Profile />} />
             </Routes>
         </main>
         <h1>Hello Fimpllor</h1>
