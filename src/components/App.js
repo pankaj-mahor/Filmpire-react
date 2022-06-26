31ad12bffe6be55effc4150c98a5204568c830b0
pankajdev1221 <pankaj.dev1221@gmail.com>
2022-06-26 14:21:20 +0530

    Initail Routing For Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { CssBaseline } from '@mui/material'
+import { Route , Routes } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-    
-        App
+        <CssBaseline />
+
+        <main>
+            <Routes>
+                <Route path="/" element={ <h1>Home</h1>} />                   
+                <Route path="/movies" element={ <h1>Movies</h1>} />
+                <Route path="/movie/:id" element={ <h1>Movie Infromation</h1>} />
+                <Route path="/actors/:id" element={ <h1>Actorrs</h1>} />
+                <Route path="/profile/:id" element={ <h1>Profile</h1>} />
+            </Routes>
+        </main>
+        <h1>Hello Fimpllor</h1>
     </div>
   )
 }
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieInformation = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default MovieInformation
